022696cfd965184a9d76ab809ed89dbce97f90dd
zelihagunay <115551527+zelihagunay@users.noreply.github.com>
2022-10-11 15:49:23 +0300

    Initial commit

---
+++ recapDemo1/src/recapDemo1/Main.java
@@ -0,0 +1,24 @@
+package recapDemo1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int sayi1=347;
+		int sayi2=8;
+		int sayi3=99;
+		
+		int enBuyuk=sayi1;
+	if (enBuyuk<sayi2) {
+		enBuyuk=sayi2;
+		
+	}
+	if(enBuyuk<sayi3) {
+		enBuyuk=sayi3;
+		
+	}
+	System.out.println("En büyük="+enBuyuk);	
+	
+	}
+	
+
+}
---
+++ variables/src/variables/Main.java
@@ -0,0 +1,21 @@
+package variables;
+
+public class Main {
+//reusability : tekrar tekrar kullanılabilirlik
+	public static void main(String[] args) {
+		
+		int ogrenciSayisi = 12; 	
+		String mesaj = "Öğrenci sayısı : ";
+		System.out.println(mesaj + ogrenciSayisi); 	
+		System.out.println(mesaj + ogrenciSayisi);
+		System.out.println("Öğrenci Sayım: "+ogrenciSayisi);
+		System.out.println("Öğrenci Sayım: "+ogrenciSayisi);
+		System.out.println("Öğrenci Sayım: "+ogrenciSayisi);
+		
+	}
+  
+
+
+	}
+
+
