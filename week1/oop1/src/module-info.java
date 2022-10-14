9daee4d5ec1e08ce8e11c4d8e271ae63ab913d5d
zelihagunay <115551527+zelihagunay@users.noreply.github.com>
2022-10-14 13:01:30 +0300

    klasorler duzenlendi

---
+++ week1/oop1/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author zelih
+ *
+ */
+module oop1 {
+}
---
+++ week1/oop1/src/oop1/Main.java
@@ -0,0 +1,57 @@
+package oop1;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		String mesaj = "Vade oranı";
+
+		Product product1 = new Product();
+		product1.setName("Delongi Kahve Makinesi");
+		product1.setDiscount(7);
+		product1.setUnitPrice(7500);
+		product1.setUnitInStock(3);
+		product1.setImageUrl("imaje1.jpg");
+
+		Product product2 = new Product();
+		product2.setName("Smeg Kahve Makinesi");
+		product2.setDiscount(7);
+		product2.setUnitPrice(7500);
+		product2.setUnitInStock(3);
+		product2.setImageUrl("imaje3.jpg");
+
+		Product product3 = new Product();
+		product3.setName("Julide Kahve Makinesi");
+		product3.setDiscount(7);
+		product3.setUnitPrice(7500);
+		product3.setUnitInStock(3);
+		product3.setImageUrl("imaje1.jpg");
+		Product[] products = { product1, product2, product3 };
+
+		System.out.println("<ul>");
+		for (Product product : products) {
+			System.out.println("<li>" + product.getName() + "</li>");
+
+		}
+		System.out.println("</ul>");
+
+		IndividualCustomer individualCustomer = new IndividualCustomer();
+		individualCustomer.setId(1);
+		individualCustomer.setPhone("05333333333");
+		individualCustomer.setCustomerNumber("12345");
+		individualCustomer.setFirstName("Engin");
+		individualCustomer.setLastName("Demiroğ");
+
+		CorporateCustomer corporateCustomer = new CorporateCustomer();
+		corporateCustomer.setId(2);
+		corporateCustomer.setCompanyName("Kodlama.io");
+		corporateCustomer.setPhone("05444444444");
+		corporateCustomer.setTaxNumber("11111111111");
+		corporateCustomer.setCustomerNumber("54321");
+
+		Customer[] customers = { individualCustomer, corporateCustomer };
+
+		System.out.println(customers);
+	}
+
+}
